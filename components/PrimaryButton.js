67e81317e244ce3bdb2a71d3d2ac04436df6951b
nicaan <nica.an23@gmail.com>
2023-03-23 15:00:25 +0200

    Adding components and screens

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function PrimaryButton({ text }) {
+  return (
+    <View>
+      <Text>{text}</Text>
+    </View>
+  );
+}
+
+export default PrimaryButton;
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,47 @@
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.input}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton text="Reset" />
+      <PrimaryButton text="Confirm" />
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#72063c",
+    elevation: 4,
+    shadowColor: "#282828",
+    shadowOffset: { width: 1, height: 8 },
+    shadowRadius: 6,
+    shadowOpacity: 0.11,
+  },
+  input: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+    width: 50,
+  },
+});
