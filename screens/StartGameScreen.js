d004c9d29910652454d7e5e668b618ca3d267b22
nicaan <nica.an23@gmail.com>
2023-03-24 22:16:51 +0200

    Handle view change after confirm

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
@@ -23,9 +23,9 @@ function StartGameScreen() {
         [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
       );
     }
-  }
 
-  function cancelInputHandler() {}
+    onPickNumber(chosenNumber);
+  }
 
   return (
     <View style={styles.inputContainer}>
